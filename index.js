29f9c2a43445df025d45e9a3c90b6e11490c80f0
luke <luke@pop-os.localdomain>
2020-10-03 11:56:50 -0500

    console output is good

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import Light from "./lightstreamerConnection";
-const axios = require("axios");
-const express = require("express");
+import axios from "axios";
+import express from "express";
+import moment from "moment";
 const app = express();
 const prodURL = "https://ciapi.cityindex.com/TradingApi";
 const port = process.env.PORT;
+import ls from "lightstreamer-client";
+
+const LIGHTSTREAMER_SERVER_HOST = "https://push.cityindex.com";
+
+const fields = [
+  "MarketId",
+  "TickDate",
+  "Bid",
+  "Offer",
+  "Price",
+  "High",
+  "Low",
+  "Change",
+  "Direction",
+  "Delta",
+  "ImpliedVolatility",
+  "AuditId",
+  "StatusSummary",
+];
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 app.get("/", (req, res) => {
   res.send("HII!");
@@ -40,9 +60,83 @@ app.get("/markets/getmarketinfo", async (req, res) => {
         Query: marketReqQuery,
       },
     });
-console.log(marketID.data.Markets.length);
-res.json('hi')
-    // Light(MARKET_ID);
+    // console.log(marketID);
+    const items = marketID.data.Markets.map((obj) => {
+      return `PRICE.${obj.MarketId}`;
+    });
+
+    // Begin LightstreamerClient()
+    var myLSClient = new ls.LightstreamerClient(
+      LIGHTSTREAMER_SERVER_HOST,
+      "STREAMINGALL"
+    );
+
+    myLSClient.connectionDetails.setUser(process.env.APP_USERNAME);
+    myLSClient.connectionDetails.setPassword(data.Session);
+
+    myLSClient.connect();
+
+    var testSubscription = new ls.Subscription("MERGE", items, fields);
+    //   var testSubscription = new ls.Subscription("MERGE", ["QUOTES"], []);
+    testSubscription.setDataAdapter("PRICES");
+
+    //   testSubscription.setDataAdapter("PRICES");
+    testSubscription.setRequestedSnapshot("yes");
+    testSubscription.setRequestedMaxFrequency(50);
+    testSubscription.setRequestedBufferSize(100);
+
+    myLSClient.subscribe(testSubscription);
+
+    myLSClient.addListener({
+      onStatusChange: function (newStatus) {
+        console.log("Status Alert: ", newStatus);
+      },
+      onListenStart: function (listenStart) {
+        console.log("Listening Started!");
+      },
+      onServerError: function (serverError) {
+        console.log("SERVER ERROR! --> ", serverError);
+      },
+      onPropertyChange: function (propertyChange) {
+        console.log(propertyChange, "changed.");
+      },
+    });
+
+    let response = [];
+    let updateCount = 0;
+    const regExp = /\(([^)]+)\)/;
+
+    testSubscription.addListener({
+      onSubscription: function () {
+        console.log("SUBSCRIBED");
+      },
+      onUnsubscription: function () {
+        console.log("UNSUBSCRIBED");
+      },
+      onItemUpdate: function (obj) {
+        console.log(
+          `${moment(parseInt(regExp.exec(obj.getValue("TickDate"))[1])).format("MMMM Do YYYY, h:mm:ss a")} | ${
+            marketID.data.Markets[updateCount].Name
+          } - Bid: ${obj.getValue("Bid")}, Offer: ${obj.getValue(
+            "Offer"
+          )}, Price: ${obj.getValue("Price")}, High: ${obj.getValue("High")},
+          Low: ${obj.getValue("Low")}\n`
+        );
+        response.push({
+          date: "Date.now()",
+          market: marketID.data.Markets[updateCount].Name,
+          bid: obj.getValue("Bid"),
+          offer: obj.getValue("Offer"),
+          price: obj.getValue("Price"),
+          high: obj.getValue("High"),
+          low: obj.getValue("Low"),
+        });
+        if (response.length === 21) {
+          res.json(response);
+        }
+        updateCount++;
+      },
+    });
   } catch (error) {
     console.log(error);
   }
