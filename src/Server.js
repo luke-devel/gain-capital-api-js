0f94b3998f88afc1fa97134ba88352494e039f10
luke <>
2020-10-10 15:58:16 -0500

    Added POST request to place buy order within current currecny pairs

--- src/Server.js
+++ src/Server.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import express from "express";
 const app = express();
 const port = process.env.PORT;
-import { PriceStream, currentMarketData } from "./PriceStream";
+import { PriceStream, sessionID, currentMarketData } from "./PriceStream";
 
 // Data.csv file will now be constantly updated
 PriceStream(process.argv[2]);
@@ -13,7 +13,7 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send(currentMarketData.Name);
+  res.send(currentMarketData.AuditId);
 });
 
 app.get("/markets/getmarketinfo", async (req, res) => {
@@ -39,6 +39,60 @@ app.get("/markets/getmarketinfo", async (req, res) => {
     console.log("API returned.");
   } catch (error) {
     console.log("res.json(error)");
+    res.status(400);
+    res.json(error);
+  }
+});
+
+app.post("/test", async (req, res) => {
+  try {
+    const { data } = await axios({
+      method: "GET",
+      url: `https://ciapi.cityindex.com/TradingAPI/useraccount/ClientAndTradingAccount`,
+      headers: {
+        username: process.env.APP_USERNAME,
+        session: sessionID,
+      },
+    });
+    // console.log(data.TradingAccounts[0].TradingAccountId);
+    const response = await axios({
+      method: "POST",
+      url: "https://ciapi.cityindex.com/TradingAPI/order/newtradeorder",
+      headers: {
+        username: process.env.APP_USERNAME,
+        session: sessionID,
+      },
+      data: {
+        IfDone: [],
+        Direction: "buy",
+        ExpiryDateTimeUTCDate: null,
+        LastChangedDateTimeUTCDate: null,
+        OcoOrder: null,
+        Type: null,
+        ExpiryDateTimeUTC: null,
+        Applicability: null,
+        TriggerPrice: null,
+        BidPrice: currentMarketData.Bid,
+        AuditId: currentMarketData.AuditId,
+        AutoRollover: false,
+        MarketId: currentMarketData.MarketId,
+        OfferPrice: currentMarketData.Offer,
+        OrderId: 0,
+        Currency: null,
+        Quantity: 1,
+        QuoteId: null,
+        LastChangedDateTimeUTC: null,
+        PositionMethodId: 1,
+        TradingAccountId: data.TradingAccounts[0].TradingAccountId,
+        MarketName: currentMarketData.MarketName,
+        Status: null,
+        isTrade: true,
+      },
+    });
+    console.log(response.data);
+    res.status(200);
+    res.json(response.data);
+  } catch (error) {
     res.status(400);
     res.json(error);
   }
